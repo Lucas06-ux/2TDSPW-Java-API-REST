484dfd17b4ce423e80a9e4d8d99e318239fc2147
Lucas06-ux <lubgian@gmail.com>
2026-02-27 22:42:06 -0300

    criando a classe dto/request para validar as informações de classes

---
+++ src/main/java/br/com/fiap/api_rest/dto/ProdutoRequest.java
@@ -0,0 +1,21 @@
+package br.com.fiap.api_rest.dto;
+
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public record ProdutoRequest(
+        @NotBlank(message = "O nome obrigatório!")
+        @Size(min = 3, max = 150, message = "O nome deve ter entre 2 e 150 caracteres")
+        String nome,
+        @NotNull(message = "O preço é obrigatório!")
+        @DecimalMin(value = "0.99", message = "O valor mínimo deve ser 0.99")
+        BigDecimal preco,
+        LocalDate expiracao
+) {
+}
